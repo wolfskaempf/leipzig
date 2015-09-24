c7eece6de7410d721cca38830704fe87adac3b4f
wolfskaempf <tom@wolfskaempf.de>
2015-09-24 15:53:35 +0200

    add ArticleAdmin

--- leipzig/admin.py
+++ leipzig/admin.py
@@ -5,3 +5,8 @@ from .models import *
 
 
 
+class ArticleAdmin(admin.ModelAdmin):
+    """ Used to manage display options in the Django Admin """
+    list_display = ("title", "author", "author_country")
+    list_filter = ("author",)
+    search_fields = ("title", "author", "author_country", "text", "introduction", "pk")
