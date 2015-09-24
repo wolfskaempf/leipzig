138c6df94c547eb4a29d00e1e179007b56aa9fc4
wolfskaempf <tom@wolfskaempf.de>
2015-09-24 14:52:04 +0200

    add programme model

--- leipzig/models.py
+++ leipzig/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Update(models.Model):
     """Used to store updates to be shown to users"""
     text = models.TextField()
+
+class Programme(models.Model):
+    """Used to store the programme for a specific date"""
+    text = models.TextField()
+    last_updated = DateTimeField(auto_now = True,)
+    date = DateField(auto_now = False,)
