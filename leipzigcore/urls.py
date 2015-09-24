30c944154941910e28a15eb2bc711f8e887a5e53
wolfskaempf <tom@wolfskaempf.de>
2015-09-24 16:20:44 +0200

    include leipzig app urls in core urls

--- leipzigcore/urls.py
+++ leipzigcore/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('leipzig.urls', namespace="leipzig")),
     url(r'^admin/', include(admin.site.urls)),
 ]
