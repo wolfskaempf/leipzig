5819af25cede9af8aa81a997fdc9b0da959793f8
wolfskaempf <tom@wolfskaempf.de>
2015-09-25 18:40:17 +0200

    add Update to home view

--- leipzig/views.py
+++ leipzig/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, render_to_response
 
+from .models import *
 # Create your views here.
 def home(request):
     """ Serves the homepage """
-    return render_to_response("home.html")
+
+    updates = Update.objects.all()
+
+    context = {"updates": updates}
+
+    return render_to_response("home.html", context)
+
