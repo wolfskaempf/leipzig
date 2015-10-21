f152b3f6bec2fbfdc5e3534c3c0dde41a3ba2b51
wolfskaempf <tom@wolfskaempf.de>
2015-10-21 18:08:12 +0200

    add first test

--- leipzig/tests.py
+++ leipzig/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser, User
+
+from views import *
+from .models import *
 # Create your tests here.
+
+
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        return 0
+
+    def test_home_view__with_empty_database(self):
+        """
+        If the database is empty, the home page should still load properly.
+        """
+        response = self.client.get(reverse("leipzig:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There is no programme available for today.")
+        self.assertContains(response, "There are currently no updates.")
+        self.assertContains(response, "There is no photo of the day.")
+
+
