dcbbaef1ed6867507289aea9d963c1589cccbf53
wolfskaempf <tom@wolfskaempf.de>
2015-10-31 23:26:17 +0100

    add error message when there are no houses, close #36

--- leipzig/tests.py
+++ leipzig/tests.py
@@ -63,6 +63,17 @@ class ProgrammeTestView(TestCase):
         self.assertContains(response, "There is no programme to be shown.")
 
 
+class HouseTestView(TestCase):
+
+    def test_house_view_with_no_houses(self):
+        """
+        If there are no houses the page should still load and an error message should be shown
+        """
+        response = self.client.get(reverse("leipzig:houses"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no houses to be shown.")
+
+
 class StaticViewTests(TestCase):
 
     def test_dictionary_view(self):
