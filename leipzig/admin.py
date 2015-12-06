18efb6201828f25eecc78102f3a041b78fe31899
wolfskaempf <tom@wolfskaempf.de>
2015-12-06 22:38:04 +0100

    make admin.py imports specific

--- leipzig/admin.py
+++ leipzig/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import Update, Programme, OfTheDay, SongWish, Article, Comment, Link, Feedback, House, Topic, Setting
 
 
 class LinkAdmin(admin.ModelAdmin):
