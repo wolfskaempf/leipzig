4ad2f288ce482163d720d7ffbd422346805a2e1a
wolfskaempf <tom@wolfskaempf.de>
2015-12-17 19:35:03 +0200

    update virtualenv

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tom/GitHub/leipzig"
+set -gx VIRTUAL_ENV "/Users/tom/GitHub/webleipzig"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
